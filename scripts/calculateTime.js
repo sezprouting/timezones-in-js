3c2c30a9d116159a6d4c4e4a2baae7d78567adaa
sezprouting <sprout3@gmail.com>
2018-02-11 10:54:25 +1300

    Add form and implement basic js to update output text

--- scripts/calculateTime.js
+++ scripts/calculateTime.js
@@ -1,5 +1,12 @@
-var submitBtn = document.getElementById('submitButton');
+document.getElementById('submitButton').onclick = function() {
+    obsvrTimeZone = document.getElementById('observer-zone-selector');
+    obsvrStartDate = document.getElementById('observer-start-date');
+    obsvrStartTime = document.getElementById('observer-start-time');
 
-submitBtn.onclick = function() {
-    alert('You pushed me!');
+    playerTimeZone = document.getElementById('player-zone-selector');
+    playerStartDate = document.getElementById('player-start-date');
+    playerStartTime = document.getElementById('player-start-time');
+
+    obsvrStartDate.textContent = 'Date: ' + playerStartDate.value;
+    obsvrStartTime.textContent = 'Time: ' + playerStartTime.value;
 }
